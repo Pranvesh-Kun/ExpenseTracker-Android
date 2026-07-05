d63a1e19352c2188eec10c62db06203b09115b1e
Pranvesh <pranveshrox@gmail.com>
2026-07-05 07:46:30 +0530

    Initial UI Finished

--- app/src/main/java/com/pranv/expensetracker/MainActivity.kt
+++ app/src/main/java/com/pranv/expensetracker/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.pranv.expensetracker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.border
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -21,8 +24,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pranv.expensetracker.ui.theme.ExpenseTrackerTheme
 
@@ -40,6 +41,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExpenseEntryScreen() {
 
@@ -51,23 +53,92 @@ fun ExpenseEntryScreen() {
         mutableStateOf("")
     }
 
+    val categories = listOf("Food", "Travel", "Shopping", "Medical", "Education", "Utilities", "Others")
+
+    var selectedCategory by remember {
+        mutableStateOf("")
+    }
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Column(modifier = Modifier.fillMaxSize().padding(vertical = 55.dp, horizontal = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(
+            modifier = Modifier.size(165.dp)
+        )
         Text(
             text = "Expense Tracker",
             modifier = Modifier
+        )
+        Spacer(
+            Modifier.size(16.dp)
         )
         TextField(
             value = merchant,
             onValueChange = { newText ->
                 merchant = newText
-            }
+            },
+            label = { Text(text = "Merchant: ", modifier = Modifier) }
+        )
+        Spacer(
+            modifier = Modifier.size(20.dp)
         )
         TextField(
             value = amount,
             onValueChange = { newText ->
                 amount = newText
-            }
+            },
+            label = { Text(text = "Amount: ", modifier = Modifier) }
         )
+        Spacer(
+            modifier = Modifier.size(20.dp)
+        )
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {
+            OutlinedTextField(
+                onValueChange = {},
+                value = selectedCategory,
+                modifier = Modifier.menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable),
+                label = {
+                    Text(text = "Select a Category:")
+                },
+                readOnly = true
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = {
+                    expanded = false
+                }
+            ) {
+                categories.forEach { item ->
+                    DropdownMenuItem(
+                        text = {
+                            Text(text = item)
+                        },
+                        onClick = {
+                          selectedCategory = item
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+        Spacer(
+            Modifier.size(16.dp)
+        )
+        Button(
+            onClick = {
+                Log.d("Check","Merchant: $merchant")
+                Log.d("Check","Amount: $amount")
+            }
+        ) {
+            Text(
+                text = "Add Expense"
+            )
+        }
     }
 }
